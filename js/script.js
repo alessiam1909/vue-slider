df09ed08419f7a03e92dfb7412a6c877219e2e31
alessiam1909 <ale.menon.vr@gmail.com>
2022-12-19 21:21:01 +0100

    creazione thumbnails con classe active al click

--- js/script.js
+++ js/script.js
@@ -37,6 +37,19 @@ createApp({
         {
             this.activeImage = index;
         },
+        //aggiungo i click sulle frecce
+        next(){
+            this.activeImage++;
+            if(this.activeImage > this.slides.length -1){
+                this.activeImage = 0;
+            }
+        },
+        prev(){
+            this.activeImage--;
+            if(this.activeImage < 0){
+                this.activeImage = this.slides.length - 1;
+            }
+        }
 
     }
 }).mount('#app')
